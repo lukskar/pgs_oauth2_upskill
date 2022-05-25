64b2ce3282b68674c4d9fb06fdf26db196b6fdcb
Lukasz Skarzynski <lskarzynski@pgs-soft.com>
2022-05-25 18:50:32 +0200

    ToDo REST API init commit

---
+++ todolists/src/main/java/eu/lukskar/upskill/todolists/ToDoListsApplication.java
@@ -0,0 +1,13 @@
+package eu.lukskar.upskill.todolists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoListsApplication.class, args);
+	}
+
+}
---
+++ todolists/src/test/java/eu/lukskar/upskill/todolists/ToDoListsApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.lukskar.upskill.todolists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
